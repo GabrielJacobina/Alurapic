db7172b7dcefc0deaa54d2ec29a14a4fb96f7a34
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-05-01 21:50:06 -0300

    Adding event Binding for filter with pipe

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -10,6 +10,7 @@ import {ActivatedRoute} from '@angular/router';
 export class PhotoListComponent implements OnInit {
 
   photos: any[] = [];
+  filter: string = '';
 
   constructor(
     private photoService: PhotoService,
