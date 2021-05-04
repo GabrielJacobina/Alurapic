501336f1c7d8fa444e3c89d2ab91cddc37c52798
GabrielJacobina <sgabrieljacobina@gmail.com>
2021-05-04 20:41:46 -0300

    Authenticating login with the api

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../core/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -10,13 +11,31 @@ export class SigninComponent implements OnInit {
 
   loginForm: FormGroup;
 
-  constructor( private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService) {
+  }
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
       userName: ['', Validators.required],
       password: ['', Validators.required]
     });
+  }
+
+  login() {
+
+    const userName = this.loginForm.get('userName').value;
+    const password = this.loginForm.get('password').value;
+
+    this.authService.authenticate(userName, password)
+      .subscribe(
+        () => console.log('Autenticado'),
+        error => {
+          console.log(error);
+          this.loginForm.reset();
+          alert('Invalid user name or password');
+        });
   }
 
 }
